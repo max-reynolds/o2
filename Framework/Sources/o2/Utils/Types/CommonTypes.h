fdc32ccd0488fe17b0269b74af4918f19b4239ae
Max Reynolds <reynolds2469@gmail.com>
2020-10-03 15:27:15 +0200

    Added Linux pratform to the enum

--- Framework/Sources/o2/Utils/Types/CommonTypes.h
+++ Framework/Sources/o2/Utils/Types/CommonTypes.h
@@ -39,7 +39,7 @@ namespace o2
 
 	enum class ProtectSection { Public, Private, Protected };
 
-	enum class Platform { Windows, MacOSX, iOS, Android };
+    enum class Platform { Windows, Linux, MacOSX, iOS, Android };
 
 	enum class LineType { Solid, Dash };
 
